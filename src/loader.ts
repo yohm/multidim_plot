202654442ed9f1bc9367bae1bef7712f7c02b84d
Yohsuke Murase <yohsuke.murase@gmail.com>
2015-10-10 21:24:36 +0900

    replot when another key is selected

--- src/loader.ts
+++ src/loader.ts
@@ -5,14 +5,45 @@
 class loader {
     
     private _chart: charting.chart;
+    private _data: Array<Object>;
     
     constructor(container: any) {
         this._chart = new charting.chart('#test');
-        d3.json('/test/test.json', (error:any, data: Array<any>) => {
-            var dataToPoint: (d:any)=>charting.dataPoint = (d:any) =>  {
-                return {x: d.x0, y: d.x1};
-            }
-            this._chart.update<any>(data, dataToPoint);
+        d3.json('/test/test.json', (error:any, data: Array<Object>) => {
+            this._data = data;
+            var keys: Array<string> = Object.keys( data[0] );
+            this.addOption(keys);
+            this.replot(keys[0], keys[1]);
         });
+    }
+    
+    private addOption(keys: Array<string>) {
+        var xSelect = d3.select('select#xSelect');
+        var xOptions = xSelect.selectAll('option').data(keys);
+        xOptions.enter().append('option').text( (d:string) => d );
+        var ySelect = d3.select('select#ySelect');
+        var yOptions = ySelect.selectAll('option').data(keys);
+        yOptions.enter().append('option').text( (d:string) => d );
+        
+        var getSelected = (select: d3.Selection<any>) => {
+            var selNode = select.node();
+            var selected = selNode.options[selNode.selectedIndex].value;
+            return selected;
+        }
+        var updatePlot = () => {
+            var selectedX = getSelected( xSelect );
+            var selectedY = getSelected( ySelect );
+            this.replot( selectedX, selectedY );
+        }
+        xSelect.on('change', updatePlot );
+        ySelect.on('change', updatePlot );
+    }
+    
+    private replot(xkey:string, ykey: string) {
+        console.log(xkey,ykey);
+        var dataToPoint: (d:Object)=>charting.dataPoint = (d:Object) => {
+            return {x: d[xkey], y: d[ykey]};
+        }
+        this._chart.update<Object>(this._data, dataToPoint);
     }
 }
