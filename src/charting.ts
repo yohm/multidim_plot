794e0ceec484db0f96c1b558c034bcd4776e8afc
Yohsuke Murase <yohsuke.murase@gmail.com>
2015-10-10 13:18:36 +0900

    refactoring: draw two axes

--- src/charting.ts
+++ src/charting.ts
@@ -3,42 +3,58 @@
 module charting {
 	export class chart {
 		private _group: d3.Selection<any>;
+		private _paddingLeft = 30;
+		private _paddingBottom = 30;
+		private _height: number;
 		
 		constructor(container: any) {
 			this.init(container);
-			this.draw();
 		}
 		
 		private init(container) {
-			this._group = d3.select(container).append('g');
+			var selection = d3.select(container);
+			var width = selection.node().clientWidth;
+			var height = selection.node().clientHeight;
+			this._height = height;
+			this._group = selection.append('svg')
+			    .attr({
+				    'width': width,
+				    'height': height
+			    })
+			    .append('g');
+			this.draw(width, height);
 		}
 		
-		private draw() {
-			this.drawXAxis();
-			this.drawYAxis();
+		private draw(width: number, height: number) {
+			this.drawXAxis(width);
+			this.drawYAxis(height);
 		}
 		
-		private drawXAxis() {
+		private drawXAxis(width:number) {
 			var scale = d3.scale.linear();
 			scale.domain([0,1]);
-			scale.range([0,600]);
-			var axis = d3.svg.axis();
-			axis.scale(scale)
-			  .orient('bottom');
-			this._group.append('g').call(axis);
+			scale.range([0, width - this._paddingLeft]);
+			var axis = d3.svg.axis()
+				.scale(scale)
+			    .orient('bottom');
+			this._group.append('g')
+				.call(axis)
+				.attr({
+					'transform': 'translate('+this._paddingLeft+','+(this._height-this._paddingBottom)+')'
+				});
 		}
 		
-		private drawYAxis() {
-			var scale = d3.scale.linear();
-			scale.domain([0,1]);
-			scale.range([800,0]);
+		private drawYAxis(height:number) {
+			var scale = d3.scale.linear()
+				.domain([0,1])
+				.range([height-this._paddingBottom,0]);
 			var axis = d3.svg.axis()
 			  .scale(scale)
 			  .orient('left');
 		    this._group.append('g')
 			  .call(axis)
 			  .attr({
-				  'transform': 'translate(30 0)'
+				  'transform': 'translate('+this._paddingLeft+',0)'
 			  });
 		}
 	}
